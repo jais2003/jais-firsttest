c30b62bf73db004ff50c8f6ac625a295119fdf0e
jais2003 <jaischauhan@gmail.com>
2024-07-28 11:54:39 +0530

    Made base class abstract

--- src/test/java/BaseClass.java
+++ src/test/java/BaseClass.java
@@ -1,16 +1,11 @@
-import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class BaseClass  
+public abstract class BaseClass
 {
 	DriverManager driverManger;
 	WebDriver driver ;
